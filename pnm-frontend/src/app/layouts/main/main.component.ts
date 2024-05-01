6baf33522dc915a42e5baf034db67938714318d4
Alexandros Karakaslidis <karakaslidis.a@sga.gr>
2024-05-01 17:14:05 +0300

    Small changes

--- pnm-frontend/src/app/layouts/main/main.component.ts
+++ pnm-frontend/src/app/layouts/main/main.component.ts
@@ -8,20 +8,19 @@ import { ThemeService } from '../../shared/theme.service';
   styleUrl: './main.component.scss'
 })
 export class MainComponent implements OnInit {
+  sidebarVisible: boolean = false;
 
-sidebarVisible: boolean = false;
+  constructor(
+    private themeService: ThemeService
+  ){
 
-    constructor(
-      private themeService: ThemeService
-    ){
+  }
 
-    }
+  ngOnInit() {
+  }
 
-    ngOnInit() {
-    }
-
-    changeTheme(theme: string) {
-      this.themeService.switchTheme(theme);
-    }
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+  }
 
 }
